e7216d8194fd60ed9f099c97bd1b99a7983b5896
Bonne <sandeepsingla75@gmail.com>
2025-03-12 18:30:10 +0530

    add files

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+session_start();       
+	
+	define("PRODUCT2ID", "174");
+	define("PRODUCT3ID", "173");
+	define("PRODUCT5ID", "172");
+	define("CIALIX1BOTTLE", "205");
+	define("TESTORX", "238");
+	define("CRAVEBLOCKER", "239");
+	define("NEATPLATE", "240");
+	define("APEXALMANAC", "241");
+        
+ define("FUNNEL", "Cialix Supplies");
+
+	$gateway_id = 1;
+	$store = "cialix";
+	$api_key = "dFdWVyH4qmgRbg2EqQG1QmQq9BxLrn";
+
+
+
+?>
